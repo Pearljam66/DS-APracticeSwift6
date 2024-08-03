cae3b16357583e7829d157b0697847c6fcdb7430
Sarah Clark <sarahireneclark@gmail.com>
2024-08-03 07:30:35 -0500

    Added new empty test file

---
+++ DS&A Practice/DS&A PracticeTests/SubstringWithConcatenationOfAllWordsTest.swift
@@ -0,0 +1,16 @@
+//
+//  SubstringWithConcatenationOfAllWordsTest.swift
+//  DS&A PracticeTests
+//
+//  Created by Sarah Clark on 8/3/24.
+//
+
+import Testing
+
+struct SubstringWithConcatenationOfAllWordsTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
